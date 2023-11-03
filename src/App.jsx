264cabad48a4fab4332558c2595e50ea0d4713a1
Jensone <hello@jensone.com>
2023-11-03 12:05:15 +0100

    Page contact : integration terminé

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+import Form from './components/Form'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="flex-shrink-0">
+        <Nav />
+        <section className="py-5">
+          <div className="container px-5">
+              <div className="bg-light rounded-4 py-5 px-4 px-md-5">
+                  <div className="text-center mb-5">
+                      <div className="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 mb-3"><i className="bi bi-envelope"></i></div>
+                      <h1 className="fw-bolder">Get in touch</h1>
+                      <p className="lead fw-normal text-muted mb-0">Let's work together!</p>
+                  </div>
+                  <div className="row gx-5 justify-content-center">
+                      <div className="col-lg-8 col-xl-6">
+                        <Form />
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
     </>
   )
 }
